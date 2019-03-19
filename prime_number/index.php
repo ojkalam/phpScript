884334b5c2b613ca497e891aa6444d9c6e76f1a7
Md Abul Kalam <ojkalam@gmail.com>
2019-03-20 02:49:48 +0600

    Prime number and form validation using js

---
+++ prime_number/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Prime number from 1 to N</title>
+</head>
+<body>	
+
+	<?php 
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			if (!empty($_POST['num'])) {
+				$num = $_POST['num'];
+
+				for ($i=0; $i < $num; $i++) { 
+					$flag = 0;
+					for ($j=1; $j <= $i; $j++) { 
+						if( ($i % $j) == 0 ){
+							$flag++;
+						}
+					}
+					if($flag == 2){
+						echo "$i <br>";
+					}
+					$flag = 0;
+				}
+			}
+		}
+
+	 ?>
+	<form id="myform" onsubmit="return validateForm()" action="" method="POST">
+		<input type="text" name="num" placeholder="Enter N number and get prime number 1 to N number">
+		<input type="submit" value="Get Prime Number">
+	</form>
+
+	<script>
+		function validateForm()
+		{
+			var num = document.forms['myform']['num'].value;
+
+			if (num == "") {
+
+				alert('Number field is required!');
+			}
+		}
+	</script>
+</body>
+</html>
